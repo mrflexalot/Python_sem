6713b526e883d04436763caea27c3f3a60ac948d
mrflexalot <108454829+mrflexalot@users.noreply.github.com>
2022-10-03 15:24:50 +0300

    Update hw_1.py

--- 2_homework/hw_1.py
+++ 2_homework/hw_1.py
@@ -3,7 +3,7 @@
 # - 6782 -> 23
 # - 0,56 -> 11
 
-a = input("enter the number to count it's digits: ")
+a = input("enter the number to count it's digits sum: ")
 sumnum = 0
 for numberdig in a:
     if numberdig.isdigit():
